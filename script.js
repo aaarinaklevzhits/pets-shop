7f4e0411885ffacf6ad14cc3bc9ebb650685cdff
aaarinaklevzhits <aaarinaklevzhits@gmail.com>
2023-05-07 18:21:46 +0300

    Итоговая версия
    Переделала в соответствии с  пошаговым руководством "Интернет-магазин".

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 500,
         img: "./img/1.jpeg",
+        rating: 4.4,
     },
     {
         title: "Игрушка лабиринт",
@@ -11,6 +12,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 900,
         img: "./img/2.jpeg",
+        rating: 3.1,
     },
     {
         title: "Игрушка для котят",
@@ -18,6 +20,7 @@ const items = [{
         tags: ["cat"],
         price: 300,
         img: "./img/3.jpeg",
+        rating: 5.0,
     },
     {
         title: "Миска «Котик»",
@@ -25,6 +28,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 660,
         img: "./img/4.jpeg",
+        rating: 4.7,
     },
     {
         title: "Лоток розовый",
@@ -32,6 +36,7 @@ const items = [{
         tags: ["cat"],
         price: 400,
         img: "./img/5.jpeg",
+        rating: 4.9,
     },
     {
         title: "Сухой корм для кошек",
@@ -39,6 +44,7 @@ const items = [{
         tags: ["cat"],
         price: 200,
         img: "./img/6.jpeg",
+        rating: 3.2,
     },
     {
         title: "Сухой корм для собак",
@@ -46,6 +52,7 @@ const items = [{
         tags: ["dog"],
         price: 300,
         img: "./img/7.jpeg",
+        rating: 2.9,
     },
     {
         title: "Игрушка для собак",
@@ -53,6 +60,7 @@ const items = [{
         tags: ["dog"],
         price: 500,
         img: "./img/8.jpeg",
+        rating: 3.4,
     },
     {
         title: "Лежанка",
@@ -60,6 +68,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 1500,
         img: "./img/9.jpeg",
+        rating: 4.8,
     },
     {
         title: "Поилка для собак",
@@ -67,6 +76,7 @@ const items = [{
         tags: ["dog"],
         price: 800,
         img: "./img/10.jpeg",
+        rating: 3.2,
     },
     {
         title: "Переноска",
@@ -74,6 +84,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 3500,
         img: "./img/11.jpeg",
+        rating: 3.7,
     },
     {
         title: "Поводок для собак",
@@ -81,44 +92,120 @@ const items = [{
         tags: ["dog"],
         price: 800,
         img: "./img/12.jpeg",
+        rating: 4.1,
     },
 ];
 
+let currentState = [...items];
 
 const containerForItems = document.querySelector("#shop-items");
 const itemTemplate = document.querySelector("#item-template");
+const nothingFound = document.querySelector('#nothing-found');
 
 function makeItemByTemplate(element) {
 
-    const title = element.title;
-    const description = element.description;
-    const img = element.img;
-    const price = element.price;
-    const tags = element.tags;
+    const { title, description, tags, img, price, rating } = element;
 
     const item = itemTemplate.content.cloneNode(true);
     item.querySelector('h1').textContent = title;
     item.querySelector('p').textContent = description;
-    item.querySelector('img').textContent = img;
+    item.querySelector('img').src = img;
     item.querySelector('.price').textContent = `${price}Р`;
-    item.querySelector('.tags').textContent = tags;
+
+
+    const ratingContainer = item.querySelector(".rating");
+
+    for (let i = 0; i < rating; i++) {
+        const star = document.createElement("i");
+        star.classList.add("fa", "fa-star");
+        ratingContainer.append(star);
+    }
+
+    const tagsHolder = item.querySelector(".tags");
+
+    tags.forEach((tag) => {
+        const tagItem = document.createElement("span");
+        tagItem.textContent = tag;
+        tagItem.classList.add("tag");
+        tagsHolder.append(tagItem);
+    });
 
     return item;
 }
 
-function addItems(arr) {
+
+function renderItems(arr) {
+    nothingFound.textContent = "";
+    containerForItems.innerHTML = "";
     arr.forEach((item) => {
         containerForItems.append(makeItemByTemplate(item));
     });
+    if (!arr.length) {
+        nothingFound.textContent = "Ничего не найдено";
+    }
 }
-addItems(items);
+
+function sortByAlphabet(a, b) {
+    if (a.title > b.title) {
+        return 1;
+    }
+    if (a.title < b.title) {
+        return -1;
+    }
+    return 0;
+}
 
 
+renderItems(currentState.sort((a, b) => sortByAlphabet(a, b)));
 
-//const searchButton = document.querySelector('#search-btn');
-//const searchInput = document.querySelector('#search-input');
-//const nothingFound = document.querySelector('#nothing-found');
-//searchButton.addEventListener("click", function(){
-//containerForItems.innerHTML = '';
-//const search = searchInput.value.trim().toLowerCase(); 
-//});
+const searchInput = document.querySelector("#search-input");
+const searchButton = document.querySelector("#search-btn");
+
+
+function applySearch() {
+    const searchString = searchInput.value.trim().toLowerCase();
+
+    currentState = items.filter((el) =>
+        el.title.toLowerCase().includes(searchString)
+    );
+
+    currentState.sort((a, b) => sortByAlphabet(a, b));
+
+    renderItems(currentState);
+
+    sortControl.selectedIndex = 0;
+}
+
+
+searchButton.addEventListener("click", applySearch);
+searchInput.addEventListener("search", applySearch);
+
+const sortControl = document.querySelector("#sort");
+
+sortControl.addEventListener("change", (event) => {
+    const selectedOption = event.target.value;
+    switch (selectedOption) {
+        case "expensive":
+            {
+                currentState.sort((a, b) => b.price - a.price);
+                break;
+            }
+        case "cheap":
+            {
+                currentState.sort((a, b) => a.price - b.price);
+                break;
+            }
+        case "rating":
+            {
+                currentState.sort((a, b) => b.rating - a.rating);
+                break;
+            }
+        case "alphabet":
+            {
+                currentState.sort((a, b) => sortByAlphabet(a, b));
+                break;
+            }
+    }
+
+    renderItems(currentState);
+});
